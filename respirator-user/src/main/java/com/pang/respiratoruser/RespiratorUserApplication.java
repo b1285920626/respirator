e13dd677548a2a2eb498974511be11c3c9433330
pang <1285920626@qq.com>
2020-10-06 20:54:10 +0800

    initialization

---
+++ respirator-user/src/main/java/com/pang/respiratoruser/RespiratorUserApplication.java
@@ -0,0 +1,13 @@
+package com.pang.respiratoruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespiratorUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RespiratorUserApplication.class, args);
+    }
+
+}
---
+++ respirator-user/src/test/java/com/pang/respiratoruser/RespiratorUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pang.respiratoruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RespiratorUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
